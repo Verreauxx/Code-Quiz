ad7f1eefe6d60517669607e0503acfb19698178e
Tino Nguyen <101163927+Verreauxx@users.noreply.github.com>
2022-08-25 22:05:05 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+  {
+    title: "What tag is used to underline a word or line of text?",
+    choices: ["<li>", "<ul>", "<s>", "<u>"],
+    answer: "<u>"
+  },
+  {
+    title: "What tag is used to render or transform text into an important (bold) version?",
+    choices: ["<em>", "<strong>", "<a>", "<blockquote>"],
+    answer: "<strong>"
+  },
+  {
+    title: "In JavaScript, what element is used to store multiple values in a single variable?",
+    choices: ["Strings","Functions","Variables","Arrays"],
+    answer: "Arrays"
+  },
+  {
+    title:"All HTML elements are considered what?",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  },
+  {
+    title:"What is the name of the statement that is used to exit or end a loop?",
+    choices: ["Break statement", "Conditional statement", "Falter statement", "Exit statement"],
+    answer: "Exit statement"
+  },
+
+];
